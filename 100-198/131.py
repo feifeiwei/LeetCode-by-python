9238654f21c8047596ef5800f6562b73b8cc2c14
feifeiwei <40533960+feifeiwei@users.noreply.github.com>
2018-06-27 14:29:06 +0800

    分割回文串

--- 100-198/131.py
+++ 100-198/131.py
@@ -1,1 +1,32 @@
-a
+'''
+输入: "aab"
+输出:
+[
+  ["aa","b"],
+  ["a","a","b"]
+]
+'''
+
+class Solution:
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        
+        ret = []
+        
+        def dum(s,cur):
+            if len(s)==0:
+                ret.append(list(cur))
+            
+            for i in range(len(s)):
+                tmp = s[:i+1]    
+                if tmp == tmp[::-1]:
+                    cur.append(tmp)
+                    dum(s[i+1:],cur)
+                    cur.pop()
+        dum(s,[])
+        
+        return ret
+            
