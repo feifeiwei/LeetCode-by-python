88502d864f7fbd05f89307f5848a44a03390b40b
feifeiwei <40533960+feifeiwei@users.noreply.github.com>
2018-06-27 14:28:10 +0800

    买卖股票的最佳时机

--- 100-198/121.py
+++ 100-198/121.py
@@ -1,1 +1,20 @@
-a
+'''
+输入: [7,1,5,3,6,4]
+输出: 5
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+'''
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price = float('inf')
+        max_pro = 0
+        
+        for s in prices:
+            min_price = min(s,min_price)
+            t = s - min_price
+            max_pro = max(max_pro,t)
+        return max_pro
