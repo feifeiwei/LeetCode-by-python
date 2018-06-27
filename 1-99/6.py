96dad0368027282e1bc6364d960387559676f201
feifeiwei <40533960+feifeiwei@users.noreply.github.com>
2018-06-27 14:16:53 +0800

    Update 6.py

--- 1-99/6.py
+++ 1-99/6.py
@@ -1,1 +1,35 @@
-a
+'''
+Z字形变换
+输入: s = "PAYPALISHIRING", numRows = 4
+输出: "PINALSIGYAHRPI"
+解释:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+'''
+
+class Solution:
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        L = ['']*numRows
+        step = 1
+        index = 0
+        for  x in (s):
+            
+            L[index] += x
+            
+            if index == 0:
+                step = 1
+            elif index == numRows-1:
+                step = -1 
+            index += step
+            
+return ''.join(L)
