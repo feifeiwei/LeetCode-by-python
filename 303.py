3d11efb702357752fd63802c0abc01a455ec33ed
feifeiwei <40533960+feifeiwei@users.noreply.github.com>
2018-06-27 10:46:12 +0800

    区域和检索 - 数组不可变

---
+++ 303.py
@@ -0,0 +1,23 @@
+'''
+给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+'''
+
+class NumArray:
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return sum(self.nums[i:j+1])
