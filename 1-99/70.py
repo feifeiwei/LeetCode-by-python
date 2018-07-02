3ca70190e435aaeeb025ec386154895afa20c902
feifeiwei <40533960+feifeiwei@users.noreply.github.com>
2018-07-02 18:50:59 +0800

    爬楼梯

--- 1-99/70.py
+++ 1-99/70.py
@@ -1,1 +1,20 @@
-a
+'''
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+'''
+
+class Solution:
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        if n<=1:
+            return 1
+        
+        if n > 1:
+            a, b = 0, 1
+            while n >= 1:
+                a,b = b,a+b
+                n -= 1
+            return b
