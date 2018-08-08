798f1ac4ec756042aa30e48f3181584c1ff2bfb9
feifeiwei <40533960+feifeiwei@users.noreply.github.com>
2018-08-08 15:29:58 +0800

    不同的二叉搜索树

--- 1-99/96.py
+++ 1-99/96.py
@@ -1,1 +1,22 @@
-a
+'''
+给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+'''
+#1
+class Solution:
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0]*(n+1)
+        dp[0] = 1
+        dp[1] = 1
+        
+        for i in range(2,n+1):
+            for j in range(0,i):
+                dp[i] += dp[j]*dp[i-j-1]
+        
+        print(dp)
+        return dp[n]
+ 
+#2
