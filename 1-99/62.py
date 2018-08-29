dcabec5ab4bcfade651d6b027ad336db12fa2468
feifeiwei <40533960+feifeiwei@users.noreply.github.com>
2018-08-29 11:31:06 +0800

    不同路径

--- 1-99/62.py
+++ 1-99/62.py
@@ -1,1 +1,20 @@
-a
+'''
+不同路径
+
+'''
+
+class Solution:
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[1]*n for i in range(m)]
+        
+        
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]
+        
+        return dp[i-1][j-1]
