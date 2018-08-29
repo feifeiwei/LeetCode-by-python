ee6c23bae80c4248bc00b41605c181dc73737102
feifeiwei <40533960+feifeiwei@users.noreply.github.com>
2018-08-29 17:43:41 +0800

    两个字符串的最小ASCII删除和

--- 596-/712.py
+++ 596-/712.py
@@ -31,3 +31,30 @@ class Solution:
                 else:
                     dp[i][j] = min( dp[i][j-1]+ord(s2[j-1]), dp[i-1][j]+ord(s1[i-1])  )
         return dp[len(s1)][len(s2)]
+
+    ###思路2
+    
+    ## s1,s2   找到 s2, s1 最多公共部分  然后  s2+s1 - 2*(公共部分)
+    class Solution:
+        def minimumDeleteSum(self, s1, s2):
+            """
+            :type s1: str
+            :type s2: str
+            :rtype: int
+            """
+            l = max(len(s1), len(s2))
+            if len(s1) != l:
+                s1, s2 = s2,s1
+            dummy = [0] * (len(s2)+1)
+
+            for i in range(1,len(s1)+1):  
+                tmp = [0]
+                for j in range(1,len(s2)+1):
+                    if s1[i-1] == s2[j-1]:
+                        tmp.append(dummy[j-1] + ord(s1[i-1]))
+                    else:
+                        tmp.append(max(tmp[-1], dummy[j]))
+                dummy = tmp[:]
+            return sum(map(ord,s1+s2)) - 2*dummy[-1]
+
+        
